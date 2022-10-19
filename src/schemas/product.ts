9afa6cc2455e0899c4aaf32739b70ffe19822cab
Sergey Zakatov <s.zakatov@gmail.com>
2022-10-19 13:21:34 +0300

    Updated product schema.

--- src/schemas/product.ts
+++ src/schemas/product.ts
@@ -2,7 +2,7 @@ import * as Yup from "yup";
 
 const baseSchema = {
   id: Yup.string().required(),
-  title: Yup.string().required(),
+  title: Yup.string().min(3).required(),
   description: Yup.string().default(""),
   price: Yup.number().min(0).required(),
   stocks: Yup.number().min(0).required(),
